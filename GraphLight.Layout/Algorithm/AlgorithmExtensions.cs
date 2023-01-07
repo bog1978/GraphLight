5b89600f3cf5e08ebe1d361515ec03d49aba45fc
bog1978 <bog1978@mail.ru>
2023-01-08 02:32:39 +0300

    Еще немного internal.

--- GraphLight.Layout/Algorithm/AlgorithmExtensions.cs
+++ GraphLight.Layout/Algorithm/AlgorithmExtensions.cs
@@ -2,14 +2,14 @@
 
 namespace GraphLight.Algorithm
 {
-    public static class AlgorithmExtensions
+    internal static class AlgorithmExtensions
     {
-        public static IAlgorithm RankNetworkSimplex<V, E>(this IGraph<V, E> graph)
+        internal static IAlgorithm RankNetworkSimplex<V, E>(this IGraph<V, E> graph)
             where V : IVertexDataLayered
             where E : IEdgeDataWeight
             => new RankNetworkSimplex<V, E>(graph);
 
-        public static IAlgorithm PositionNetworkSimplex<V, E>(this IGraph<V, E> graph)
+        internal static IAlgorithm PositionNetworkSimplex<V, E>(this IGraph<V, E> graph)
             where V : IVertexDataLayered, IVertexDataLocation
             where E : IEdgeDataWeight
             => new PositionNetworkSimplex<V, E>(graph);
