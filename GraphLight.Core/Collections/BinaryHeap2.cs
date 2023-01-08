1ac4ca56f94f947c69adc274a0298064e485b74f
bog1978 <bog1978@mail.ru>
2023-01-08 22:43:35 +0300

    Формат кода

--- GraphLight.Core/Collections/BinaryHeap2.cs
+++ GraphLight.Core/Collections/BinaryHeap2.cs
@@ -8,24 +8,69 @@ namespace GraphLight.Collections
     public class BinaryHeap2<TElement, TPriority> : IReadOnlyCollection<TElement>
         where TPriority : IComparable<TPriority>
     {
-        private readonly List<HeapItem> _heap;
+        #region Константы и поля
+
         private readonly int _sgn;
+        private readonly List<HeapItem> _heap;
+
+        #endregion
+
+        #region Конструкторы
 
         public BinaryHeap2(HeapType heapType)
         {
-            _sgn = heapType == HeapType.Min ? -1 : 1;
+            _sgn = heapType == HeapType.Min
+                ? -1
+                : 1;
             _heap = new List<HeapItem>();
         }
 
         public BinaryHeap2(IEnumerable<TElement> items, Func<TElement, TPriority> priorityFunc, HeapType heapType)
         {
-            _sgn = heapType == HeapType.Min ? -1 : 1;
+            _sgn = heapType == HeapType.Min
+                ? -1
+                : 1;
             var heapItems = items.Select(x => new HeapItem(x, priorityFunc(x)));
             _heap = new List<HeapItem>(heapItems);
             BuildHeap();
         }
 
-        #region ICollection<TValue> Members
+        #endregion
+
+        #region События, свойства, индексаторы
+
+        public TElement Root =>
+            _heap.Count == 0
+                ? throw new InvalidOperationException("Heap is empty.")
+                : _heap[0].Element;
+
+        #endregion
+
+        #region IEnumerable
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        #endregion
+
+        #region IEnumerable<TElement>
+
+        public IEnumerator<TElement> GetEnumerator() => new HeapEnumerator(_heap.GetEnumerator());
+
+        #endregion
+
+        #region IReadOnlyCollection<TElement>
+
+        public int Count => _heap.Count;
+
+        #endregion
+
+        #region Другое
+
+        private static int Left(int i) => (i << 1) + 1;
+
+        private static int Parent(int i) => (i - 1) >> 1;
+
+        private static int Right(int i) => (i << 1) + 2;
 
         public void Add(TElement element, TPriority priority)
         {
@@ -35,18 +80,6 @@ namespace GraphLight.Collections
                  i = p, p = Parent(i))
                 Swap(p, i);
         }
-
-        public int Count => _heap.Count;
-
-        public IEnumerator<TElement> GetEnumerator() => new HeapEnumerator(_heap.GetEnumerator());
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-        #endregion
-
-        public TElement Root => _heap.Count == 0
-            ? throw new InvalidOperationException("Heap is empty.")
-            : _heap[0].Element;
 
         public TElement RemoveRoot()
         {
@@ -64,12 +97,16 @@ namespace GraphLight.Collections
                 Heapify(i);
         }
 
+        private int Compare(int i, int j) => _sgn * _heap[i].Priority.CompareTo(_heap[j].Priority);
+
         private void Heapify(int i)
         {
             var l = Left(i);
             var r = Right(i);
             var count = _heap.Count;
-            var largest = l < count && Compare(l, i) > 0 ? l : i;
+            var largest = l < count && Compare(l, i) > 0
+                ? l
+                : i;
             if (r < count && Compare(r, largest) > 0)
                 largest = r;
             if (largest == i)
@@ -77,8 +114,6 @@ namespace GraphLight.Collections
             Swap(i, largest);
             Heapify(largest);
         }
-
-        private int Compare(int i, int j) => _sgn * _heap[i].Priority.CompareTo(_heap[j].Priority);
 
         private void Swap(int i, int j)
         {
@@ -88,42 +123,66 @@ namespace GraphLight.Collections
             _heap[j] = a;
         }
 
-        private static int Parent(int i) => (i - 1) >> 1;
-
-        private static int Left(int i) => (i << 1) + 1;
-
-        private static int Right(int i) => (i << 1) + 2;
-
-        private class HeapItem
-        {
-            public HeapItem(TElement element, TPriority priority)
-            {
-                Element = element;
-                Priority = priority;
-            }
-
-            public readonly TElement Element;
-            public readonly TPriority Priority;
-        }
+        #endregion
 
         private class HeapEnumerator : IEnumerator<TElement>
         {
+            #region Константы и поля
+
             private readonly IEnumerator<HeapItem> _enumerator;
+
+            #endregion
+
+            #region Конструкторы
 
             public HeapEnumerator(IEnumerator<HeapItem> enumerator)
             {
                 _enumerator = enumerator;
             }
 
+            #endregion
+
+            #region IDisposable
+
             public void Dispose() => _enumerator.Dispose();
+
+            #endregion
+
+            #region IEnumerator
 
             public bool MoveNext() => _enumerator.MoveNext();
 
             public void Reset() => _enumerator.Reset();
 
+            object? IEnumerator.Current => Current;
+
+            #endregion
+
+            #region IEnumerator<TElement>
+
             public TElement Current => _enumerator.Current.Element;
 
-            object? IEnumerator.Current => Current;
+            #endregion
+        }
+
+        private class HeapItem
+        {
+            #region Константы и поля
+
+            public readonly TElement Element;
+            public readonly TPriority Priority;
+
+            #endregion
+
+            #region Конструкторы
+
+            public HeapItem(TElement element, TPriority priority)
+            {
+                Element = element;
+                Priority = priority;
+            }
+
+            #endregion
         }
     }
 }
--- GraphLight.Core/Collections/PriorityQueue2.cs
+++ GraphLight.Core/Collections/PriorityQueue2.cs
@@ -7,7 +7,13 @@ namespace GraphLight.Collections
     public class PriorityQueue2<TElement, TPriority> : IReadOnlyCollection<TElement>
         where TPriority : IComparable<TPriority>
     {
+        #region Константы и поля
+
         private readonly BinaryHeap2<TElement, TPriority> _heap;
+
+        #endregion
+
+        #region Конструкторы
 
         public PriorityQueue2(HeapType heapType)
         {
@@ -17,16 +23,34 @@ namespace GraphLight.Collections
         public PriorityQueue2(IEnumerable<TElement> items, Func<TElement, TPriority> priorityFunc, HeapType heapType) =>
             _heap = new BinaryHeap2<TElement, TPriority>(items, priorityFunc, heapType);
 
-        public void Enqueue(TElement item, TPriority priority) => _heap.Add(item, priority);
+        #endregion
 
-        public TElement Dequeue() => _heap.RemoveRoot();
-
-        public TElement Peek() => _heap.Root;
-
-        public IEnumerator<TElement> GetEnumerator() => _heap.GetEnumerator();
+        #region IEnumerable
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
+        #endregion
+
+        #region IEnumerable<TElement>
+
+        public IEnumerator<TElement> GetEnumerator() => _heap.GetEnumerator();
+
+        #endregion
+
+        #region IReadOnlyCollection<TElement>
+
         public int Count => _heap.Count;
+
+        #endregion
+
+        #region Другое
+
+        public TElement Dequeue() => _heap.RemoveRoot();
+
+        public void Enqueue(TElement item, TPriority priority) => _heap.Add(item, priority);
+
+        public TElement Peek() => _heap.Root;
+
+        #endregion
     }
 }
