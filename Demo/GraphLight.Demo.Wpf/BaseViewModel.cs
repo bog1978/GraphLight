ba4cffe12d5a600639f671cd70f0a92e9b55b3c6
bog1978 <bog1978@mail.ru>
2025-07-01 19:11:35 +0300

    nullable

--- Demo/GraphLight.Demo.Wpf/BaseViewModel.cs
+++ Demo/GraphLight.Demo.Wpf/BaseViewModel.cs
@@ -5,12 +5,12 @@ namespace GraphLight.Demo
 {
     public abstract class BaseViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected void RaisePropertyChanged([CallerMemberName] string name = null) => 
+        protected void RaisePropertyChanged([CallerMemberName] string? name = null) => 
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
-        protected void SetProperty<T>(ref T field, T value, [CallerMemberName] string name = null)
+        protected void SetProperty<T>(ref T field, T value, [CallerMemberName] string? name = null)
         {
             if(Equals(field, value))
                 return;
